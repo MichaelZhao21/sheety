8af5acb2671b7a23c61340514413a353e846ca66
Michael Zhao <michaelzhao314@gmail.com>
2024-10-15 20:18:26 -0500

    Added tempo to env with checks

--- dev/old/main.py
+++ dev/old/main.py
@@ -8,6 +8,7 @@ url = os.getenv('URL')
 print('URL:', url)
 max_seconds = int(os.getenv('MAX_SECONDS'))
 print('MAX_SECONDS:', max_seconds)
+raw_tempo = os.getenv('TEMPO')
 
 # tempo, maxSeconds
 # Download the video
@@ -16,6 +17,9 @@ dl_res = download_video(url)
 print('Downloaded video:', dl_res['title'])
 print('Tempo:', dl_res['tempo'])
 print('Max seconds:', max_seconds)
+
+if raw_tempo:
+    dl_res['tempo'] = int(raw_tempo)
 
 # Extract the notes
 notes = extract_notes(dl_res['file_path'], dl_res['tempo'], int(max_seconds))
